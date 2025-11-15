ddc406503bed712a3923bbc8430b5be3cb35c9dd
ahmed <ahmedmasry5464222@gmail.com>
2025-11-15 21:18:31 +0200

    users classes added

---
+++ src/model/User.java
@@ -0,0 +1,14 @@
+package model;
+
+public class User {
+
+    //Attributes for all users
+    private String userId;
+    private String role;
+    private String username;
+    private String email;
+    private String passwordHash;
+
+
+
+}
