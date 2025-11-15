e7a9bcc38d10b4acd8f58cc7dc86e05913d9bc34
Abdelrahman El-Sharnouby <bodaelsharnouby80@gmail.com>
2025-11-15 18:32:33 +0200

    Created Course and Lesson Classes

---
+++ src/model/Course.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Course {
+
+    private String courseID;
+    private String title;
+    private String description;
+    private String instructorID;
+    private List<Lesson> lessons;
+
+
+    public Course(String courseID, String title, String description, String instructorID, List<Lesson> lessons) {
+        this.courseID = courseID;
+        this.title = title;
+        this.description = description;
+        this.instructorID = instructorID;
+        this.lessons = lessons;
+    }
+
+    public void addLesson(Lesson lesson){
+        this.lessons.add(lesson);
+    }
+
+    public void removeLesson(Lesson lesson){
+        this.lessons.remove(lesson);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getInstructorID() {
+        return instructorID;
+    }
+
+    public void setInstructorID(String instructorID) {
+        this.instructorID = instructorID;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+}
---
+++ src/model/Lesson.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Lesson {
+
+
+    private String lessonID;
+    private String title;
+    private String content;
+    private boolean isCompleted;
+
+
+    public Lesson(String lessonID, String title, String content) {
+        this.lessonID = lessonID;
+        this.title = title;
+        this.content = content;
+        this.isCompleted = false;
+    }
+
+    public String getLessonID() {
+        return lessonID;
+    }
+
+    public void setLessonID(String lessonID) {
+        this.lessonID = lessonID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted() {
+        this.isCompleted = true;
+    }
+}
+
