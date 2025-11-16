fb6869235308ae6bdcb7bde26dfa141facb95528
Abdelrahman El-Sharnouby <bodaelsharnouby80@gmail.com>
2025-11-16 20:08:21 +0200

    Created Validator Class

--- src/requirements/Validator.java
+++ src/requirements/Validator.java
@@ -1,4 +1,24 @@
 package requirements;
 
 public class Validator {
+
+    public static boolean validEmail(String email) {
+        if(email == null) {
+            return false;
+        }
+        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        return email.matches(regex);
+    }
+
+    public static boolean validUserName(String userName) {
+        if(userName == null || userName.length() < 3){
+            return false;
+        }
+        String regex = "^[A-Za-z0-9_-]{3,}";
+        return userName.matches(regex);
+    }
+
+    public static boolean validPassword(String password) {
+        return password != null && password.length() >= 8;
+    }
 }
