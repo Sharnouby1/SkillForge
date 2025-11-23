89ca5e74d1c6b50841e74c05f7dfd659c1d80467
ahmedwael <dodowael0@gmail.com>
2025-11-23 18:30:19 +0200

    enrollment

--- src/gui/AccessCourse.java
+++ src/gui/AccessCourse.java
@@ -2,15 +2,18 @@ package gui;
 
 import javax.swing.*;
 import database.*;
-import model.Course;
 import model.Student;
 import service.AuthService;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.List;
 
 public class AccessCourse extends JFrame {
     private JPanel container1;
     private JList<String>list1;
+    private JButton accessButton;
+
     public AccessCourse() {
         setTitle("Access Course");
         setContentPane(container1);
@@ -23,10 +26,21 @@ public class AccessCourse extends JFrame {
         Student s = AuthService.getLoggedStudent();
         List<String> enrolledTitles = db.getEnrolledCourseTitles(s.getUserId());
 
-
         list1.setListData(enrolledTitles.toArray(new String[0]));
 
 
+        accessButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedCourse = list1.getSelectedValue();
+                if (selectedCourse == null) {
+                    JOptionPane.showMessageDialog(null, "Please select a course first!");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Accessing Course: " + selectedCourse);
+
+                }
+            }
+        });
     }
 }
 
